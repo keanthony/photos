9df408323998a5d24c69389b62f16e072ec96ec0
keanthony <kanthony@ggc.edu>
2016-01-20 18:11:59 +0000

    hard reset

--- git.sh
+++ git.sh
@@ -1,6 +1,19 @@
 #!\bin\bash#
 git add -A
-git commit -m "added bio"
-git push
+git commit -m "hard reset"
+git push origin gh-pages
 #keanthony
 #HzMRKZqa24AX
+#nano, more, less - file editor
+#touch - create file
+#source - run shell file
+#ls - show files in folder.  -a show all including hidden files
+#git checkout - revert file to the committed version in repository
+#git checkout - switches to branch
+#git reset --soft HEAD~1 - undo last commit that hasnt been pushed
+#git reset --hard HEAD~1 - undo last push (lose data) "1" is the number
+#   of pushes to roll back
+#git clone - clones new repository
+#clear - clears the console
+#git branch - creates a branch in repository
+#git push origin - name 
